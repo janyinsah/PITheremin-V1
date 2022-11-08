c448436fccff2d07bc04dd60076506b20680548f
janyinsah <ja.bondzie@aol.com>
2022-11-08 22:34:43 +0000

    Main has been temporarily removed. Generate Sound has been encapsulated into a class module.

--- generate_sound.py
+++ generate_sound.py
@@ -4,23 +4,23 @@ import numpy as np
 import pyaudio
 import generate_wave
 
+class generateSound:
 
-aud = pyaudio.PyAudio() #Define pyaudio product module
+    aud = pyaudio.PyAudio() #Define pyaudio product module
 
+    class SoundAudio(generate_wave.AudioSamples):
+        volume = 0.5 #Initial volume set volume
+        duration = 5.0 #How long the sine wave will play.
 
-class SoundAudio(generate_wave.AudioSamples):
-    volume = 0.5 #Initial volume set volume
-    duration = 5.0 #How long the sine wave will play.
-
-num_samples = int(generate_wave.AudioSamples.freq * SoundAudio.duration)
-samples = (SoundAudio.volume * generate_wave.gen_sine_wave().astype(np.float32))
+    num_samples = int(generate_wave.AudioSamples.freq * SoundAudio.duration)
+    samples = (SoundAudio.volume * generate_wave.gen_sine_wave().astype(np.float32))
 
 #Convert to bytes
-output_bytes = (SoundAudio.volume * samples).tobytes()
+    output_bytes = (SoundAudio.volume * samples).tobytes()
 
 #Create volume stream uplaod to pyaudio file format.
-stream = aud.open(format = pyaudio.paFloat32, channels = 1, rate = generate_wave.AudioSamples.s_rate, output = True)
+    stream = aud.open(format = pyaudio.paFloat32, channels = 1, rate = generate_wave.AudioSamples.s_rate, output = True)
 
-stream.write((SoundAudio.volume * samples).tobytes())
+    stream.write((SoundAudio.volume * samples).tobytes())
 
 

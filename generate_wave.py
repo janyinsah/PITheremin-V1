f4715d3076886f5577f4f0dd98ec47845ca4b0dd
janyinsah <ja.bondzie@aol.com>
2022-11-08 16:30:24 +0000

    In-case of future additions of weveforms and merging, the existing methods have been refined into functions These will be called in the main.py file.

--- generate_wave.py
+++ generate_wave.py
@@ -2,12 +2,14 @@ import numpy as np
 import pyaudio
 from scipy import signal
 
-s_rate = 48000 #Sampling Rate
-freq= 448 #Frequency (Hz)
-samples = 48000 #Number of samples
-x = np.arange(samples)
+def gen_sine_wave(sine_y):
+    
+    s_rate = 44100 #Sampling Rate
+    freq= 440.0 #Frequency (Hz)
+    samples= 44100 #Number of samples
+    x = np.arange(samples)
 
-#Generating the Sound Wave (Sine) for the PI Theremin.
-sine_y = 100*np.sin(2 * np.pi * freq * x / s_rate)
-
+    #Generating the Sound Wave (Sine) for the PI Theremin.
+    sine_y = 100*np.sin(2 * np.pi * freq * x / s_rate)
+    return sine_y
 

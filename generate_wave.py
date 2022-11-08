f9b724846d494173a01f9960174c1fd060b465de
janyinsah <ja.bondzie@aol.com>
2022-11-08 19:49:20 +0000

    The sampling rate has been refined into a class to define attributes for the audio sampling rates. The class attributes will be used for future use if more waveforms are generated.

--- generate_wave.py
+++ generate_wave.py
@@ -2,14 +2,15 @@ import numpy as np
 import pyaudio
 from scipy import signal
 
-def gen_sine_wave(sine_y):
-    
+class AudioSamples: 
+    #This is a class to define the sampling attributes for each wave generated for the PI Theremin. 
     s_rate = 44100 #Sampling Rate
     freq= 440.0 #Frequency (Hz)
     samples= 44100 #Number of samples
     x = np.arange(samples)
 
+def gen_sine_wave(sine_y):
+    
     #Generating the Sound Wave (Sine) for the PI Theremin.
-    sine_y = 100*np.sin(2 * np.pi * freq * x / s_rate)
+    sine_y = 100*np.sin(2 * np.pi * AudioSamples.freq * AudioSamples.x / AudioSamples.s_rate)
     return sine_y
-

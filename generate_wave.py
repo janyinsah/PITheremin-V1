1971428b108c542b15307bcc5a99a80e989929fc
janyinsah <ja.bondzie@aol.com>
2022-11-09 20:43:37 +0000

    main.py initialized.

--- generate_wave.py
+++ generate_wave.py
@@ -2,6 +2,7 @@ import numpy as np
 import pyaudio
 from scipy import signal
 
+
 class AudioSamples: 
     #This is a class to define the sampling attributes for each wave generated for the PI Theremin. 
     s_rate = 44100 #Sampling Rate
@@ -14,4 +15,6 @@ def gen_sine_wave():
     #Generating the Sound Wave (Sine) for the PI Theremin.
     sine_y = 100*np.sin(2 * np.pi * AudioSamples.freq * AudioSamples.x / AudioSamples.s_rate)
     return sine_y
+
+
 
---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy
+import mediapipe
+import cv2
+import generate_sound
+import generate_wave
+import handtracking#
+
+def main():
+    print("PI Theremin loaded.")
+
+if __name__ == "__main__":
+    main()

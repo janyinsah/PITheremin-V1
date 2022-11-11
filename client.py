179d6ae1c6b7fac4cc9d0ea59e6607178eabebcb
janyinsah <114355679+janyinsah@users.noreply.github.com>
2022-11-11 04:58:14 +0000

    Client file initialized.

--- client.py
+++ client.py
@@ -1,12 +1,18 @@
+#This creates a UPD client in which will allow python scripts to talk with the sonic pi client.
+
 import time
 import random
 
 from pythonosc import udp_client
 
-client = udp_client.SimpleUDPClient('127.0.0.1', 4559)
+class client_attributes:
+    client = udp_client.SimpleUDPClient('127.0.0.1', 4559)
 
-while True:
-    client.send_message("/bpm", random.randint(40,200))
-    time.sleep(5)
+def create_client():
+    while True:
+        client_attributes.send_message("/bpm", random.randint(40,200))
+        time.sleep(5)
+
+
 
     
---
+++ generate_tone.py
@@ -0,0 +1,6 @@
+# Function which generates tone for the Pi Theremin.
+import psonic
+import pythonosc
+import client
+
+# Create function which generates a waveform tone for the PI Theremin.

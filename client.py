23c9d1970e2914a1990b07e84afa6ed1b7f80f7d
janyinsah <ja.bondzie@aol.com>
2023-03-02 01:26:55 +0000

    New file structure.

---
+++ client.py
@@ -0,0 +1,13 @@
+#----------------------------------------------------------------
+#Import Modues for the client file.
+import argparse
+import random
+import time
+
+from pythonosc import udp_client
+
+# Function to connect supported 
+def connect_to_client(ip, port):
+    client = udp_client.SimpleUDPClient(ip, port)
+    return client
+
---
+++ main.py
@@ -0,0 +1,1 @@
+# PI Theremin main program.
